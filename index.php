9978d50e7e9cd2544b11ce5e0dcb55de967772a1
Bekhruz_Sharapov <BekhruzSHofficial@gmail.com>
2025-10-12 22:38:49 +0500

    associative arrays - arrays inside arrays with keys & values

--- index.php
+++ index.php
@@ -11,16 +11,28 @@
     <h1>Recommended books</h1>
 
     <?php
+
     $books = [
-        "Do Androids dream of Electric Sheeps",
-        "Langoliers",
-        "Hail Mary"
+        [
+            "name" => "Do Androids dream of Electric Sheeps",
+            "author" => "Philip K. Dick",
+            "purchaseUrl" => "https://google.com"
+        ],
+        [
+            "name" => "The Langoliers",
+            "author" => "James Bond",
+            "purchaseUrl" => "https://linkedin.com"
+        ]
     ];
     ?>
 
     <ul>
         <?php foreach ($books as $book) : ?>
-            <li><?= $book; ?></li>
+            <li>
+                <a href="<?= $book['purchaseUrl']; ?>">
+                    <?= $book['name']; ?>
+                </a>
+            </li>
         <?php endforeach; ?>
     </ul>
 
