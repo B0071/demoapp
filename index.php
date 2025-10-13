011691c9928a2435782c92a52098a03c8f538f09
Bekhruz_Sharapov <BekhruzSHofficial@gmail.com>
2025-10-13 20:28:52 +0500

    added about us & contact page

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
 <?php
 
-$books = [
-    [
-        "name" => "Do Androids dream of Electric Sheeps",
-        "author" => "Philip K. Dick",
-        "releaseYear" => 2025,
-        "purchaseUrl" => "https://google.com"
-    ],
-    [
-        "name" => "The Langoliers",
-        "author" => "Andy Weir",
-        "releaseYear" => 2021,
-        "purchaseUrl" => "https://linkedin.com"
-    ],
-    [
-        "name" => "The Martians",
-        "author" => "Andy Weir",
-        "releaseYear" => 2011,
-        "purchaseUrl" => "https://linkedin.com"
-    ]
-];
-
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['releaseYear'] < 2022;
-});
-
 require 'index.view.php';
