948cc991f04cc3691663419f997e0fe4103690fe
Bekhruz_Sharapov <BekhruzSHofficial@gmail.com>
2025-10-19 18:59:57 +0500

    cleaned up for notes mini project

--- index.php
+++ index.php
@@ -4,22 +4,8 @@ require 'functions.php';
 
 require 'Database.php';
 
-// require 'router.php';
+require 'router.php';
 
 $config = require('config.php');
 
 $db = new Database($config['database']);
-
-$id = $_GET['id'];
-
-$query = "select * from posts where id = :id";
-
-$post = $db->query($query, [
-    ':id' => $id
-])->fetch();
-
-dd($post);
-
-// foreach ($posts as $post) {
-//     echo '<li>' . $post['title'] . '</li>';
-// }
