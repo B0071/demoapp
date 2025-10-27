be19d53331e9fdf95ffd782975191b23b499cae1
Bekhruz_Sharapov <BekhruzSHofficial@gmail.com>
2025-10-27 19:26:54 +0500

    added form validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,14 +6,28 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query(
-        "INSERT INTO notes (title, context, user_id) VALUES (:title, :context, :user_id)",
-        [
-            ':title' => $_POST['title'],
-            ':context' => $_POST['context'],
-            ':user_id' => 1
-        ]
-    );
+
+    $errors = [];
+
+    // title shouldn't be empty
+    if (strlen($_POST['title']) === 0) {
+        $errors['title'] = "Note's title cannot be empty";
+    }
+
+    if (strlen($_POST['title']) > 100) {
+        $errors['title'] = "Note's title cannot be longer than 100 characters.";
+    }
+
+    if (empty($errors)) {
+        $db->query(
+            "INSERT INTO notes (title, context, user_id) VALUES (:title, :context, :user_id)",
+            [
+                ':title' => $_POST['title'],
+                ':context' => $_POST['context'],
+                ':user_id' => 1
+            ]
+        );
+    }
 }
 
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -8,12 +8,15 @@
                     <div class="space-y-12">
                         <div class="border-b border-gray-900/10 pb-12">
                             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                                <div class="sm:col-span-2">
+                                <div class="sm:col-span-3">
                                     <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
                                     <div class="mt-2">
-                                        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                            <input id="title" type="text" name="title" class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
-                                        </div>
+                                        <textarea name="title" id="title">
+                                            <?= isset($_POST['title']) ? $_POST['title'] : '' ?>
+                                        </textarea>
+                                        <?php if (isset($errors['title'])) : ?>
+                                            <p class="text-red-500 text-xs mt-2"><?= $errors['title']; ?></p>
+                                        <?php endif; ?>
                                     </div>
                                 </div>
                                 <br>
