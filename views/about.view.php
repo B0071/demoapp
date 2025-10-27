ba83dcb3359c888639ff4cb835490e12f72313d1
Bekhruz_Sharapov <BekhruzSHofficial@gmail.com>
2025-10-27 20:20:52 +0500

    nothing

--- views/about.view.php
+++ views/about.view.php
@@ -1,8 +1,5 @@
         <?php require('views/partials/head.php'); ?>
-
         <?php require('views/partials/nav.php'); ?>
-
-
         <?php require('views/partials/banner.php'); ?>
 
         <main>
