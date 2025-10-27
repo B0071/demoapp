6177def96233554e3379235b5a00161c067ca819
Bekhruz_Sharapov <BekhruzSHofficial@gmail.com>
2025-10-27 12:59:37 +0500

    refactored POD fetch method with our wrapper find & findOrFail methods

--- controllers/note.php
+++ controllers/note.php
@@ -1,19 +1,14 @@
 <?php
 
 $heading = "My Note";
+$currentUser = 1;
 
 $config = require('config.php');
 $db = new Database($config['database']);
 
 $note = $db->query("select * from notes where id = :id", [
     ':id' => $_GET['id']
-])->fetch();
-
-if (! $note) {
-    abort();
-}
-
-$currentUser = 1;
+])->findOrFail();
 
 if ($note['user_id'] !== $currentUser) {
     abort(Response::FORBIDDEN);
