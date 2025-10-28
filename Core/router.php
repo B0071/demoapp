0ac1f22ef61f45e75031f29902b3a4562cf90c47
Bekhruz_Sharapov <BekhruzSHofficial@gmail.com>
2025-10-28 19:32:22 +0500

    moved functions & router file to Core folder

--- Core/router.php
+++ Core/router.php
@@ -2,14 +2,14 @@
 
 $url = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$routes = require 'routes.php';
+$routes = require base_path('routes.php');
 
 // redirects to corresponding controller
 function redirectToController($url, $routes)
 {
     // checks whether URL exists in $routes array
     if (array_key_exists($url, $routes)) {
-        require $routes[$url];
+        require base_path($routes[$url]);
     } else {
         // if not runs the function below which is 
         abort();
@@ -19,7 +19,7 @@ function redirectToController($url, $routes)
 function abort($code = 404)
 {
     http_response_code($code);
-    require "views/{$code}.php";
+    require base_path("views/{$code}.php");
     die();
 }
 
