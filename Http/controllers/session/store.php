f91f30cdf542664a06820c1647cedc9c3d9f70b7
Bekhruz_Sharapov <BekhruzSHofficial@gmail.com>
2025-11-03 21:39:45 +0500

    organized files & added LoginForm class to extract validation

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -3,6 +3,7 @@
 use Core\App;
 use Core\Database;
 use Core\Validator;
+use Http\Forms\LoginForm;
 
 $email = $_POST['email'];
 $password = $_POST['password'];
@@ -11,19 +12,11 @@ $db = App::resolve(Database::class);
 
 // validate inputs
 
-$errors = [];
+$form = new LoginForm();
 
-if (!Validator::email($email)) {
-    $errors['email'] = 'Please provide a valid email address.';
-}
-
-if (!Validator::string($password)) {
-    $errors['password'] = 'Please provide a valid password. Password cannot be less than 7 characters.';
-};
-
-if (!empty($errors)) {
+if (! $form->validate($email, $password)) {
     return view('session/create.view.php', [
-        'errors' => $errors
+        'errors' => $form->errors()
     ]);
 }
 
